ca59a6671baeebe4fffbf9ecacefcb8c25dcb969
Guudz <122683344+Guudz@users.noreply.github.com>
2023-01-14 17:03:13 +0100

    Initial commit

---
+++ ESX_DRUGS/esx_drugs/config.lua
@@ -0,0 +1,33 @@
+Config              = {}
+Config.MarkerType   = -1
+Config.DrawDistance = 100.0
+Config.ZoneSize     = {x = 4.0, y = 4.0, z = 4.0}
+Config.MarkerColor  = {r = 224, g = 224, b = 224}
+
+Config.RequiredCopsCoke  = 2
+Config.RequiredCopsMeth  = 2
+Config.RequiredCopsWeed  = 0
+Config.RequiredCopsOpium = 2
+
+Config.TimeToFarm    = 5 * 1000
+Config.TimeToProcess = 5 * 1000
+Config.TimeToSell    = 5  * 1000
+
+Config.Locale = 'fr'
+
+Config.Zones = {
+	CokeField =			{x = 1911.56,	y = 4810.318,	z = 42.84,	name = _U('coke_field'),		sprite = 501,	color = 40},
+	CokeProcessing =	{x = 519.60,	y = 3085.27,	z = 41.00,	name = _U('coke_processing'),	sprite = 478,	color = 40},
+	CokeDealer =		{x = 279.32,	y = 6792.66,		z = 15.92,	name = _U('coke_dealer'),		sprite = 500,	color = 75},
+	MethField =			{x = 1429.9492,	y = 4368.6025,	z = 44.60,	name = _U('meth_field'),		sprite = 499,	color = 26},
+	MethProcessing =	{x = 1978.735,	y = -2607.87,	z = 3.552,	name = _U('meth_processing'),	sprite = 499,	color = 26},
+	MethDealer =		{x = -1077.982,	y = -1675.88,	z = 4.57,	name = _U('meth_dealer'),		sprite = 500,	color = 75},
+	WeedField =		    {x = 1254.351,	y =-1987.989,	z = 43.37,	name = _U('weed_field'),		sprite = 496,	color = 52},
+	WeedProcessing =	{x = 432.785,	y = 6468.867,	z = 35.86,	name = _U('weed_processing'),	sprite = 496,	color = 52},
+	WeedDealer =		{x = -510.099,	y = 52.320,	z = 44.75,	name = _U('weed_dealer'),		sprite = 500,	color = 75},
+	OpiumField =		{x = 1122.71,	y = -3194.5,	z = -41.39,	name = _U('opium_field'),		sprite = 51,	color = 60},
+	OpiumProcessing =	{x = -475.12,	y = 6288.06,	z = 12.67,	name = _U('opium_processing'),	sprite = 51,	color = 60},
+	OpiumDealer =		{x = -1001.42,	y = 4847.92,	z = 274.07,	name = _U('opium_dealer'),		sprite = 500,	color = 75}
+}
+
+
---
+++ ESX_DRUGS/esx_teleports/config.lua
@@ -0,0 +1,160 @@
+Config                            = {}
+
+Config.Teleporters = {
+	--[['EclipseTowers'] = {
+		['Job'] = 'police',
+		['Enter'] = { 
+			['x'] = -777.03137207031, 
+			['y'] = 319.640625, 
+			['z'] = 84.717643432617,
+			['Information'] = '[E] Go Up',
+		},
+		['Exit'] = {
+			['x'] = -781.83642578125, 
+			['y'] = 325.7190246582, 
+			['z'] = 175.85869567871, 
+			['Information'] = '[E] Go Down' 
+		}
+	},
+
+	['Cardealer'] = {
+		['Job'] = 'none',
+		['Enter'] = { 
+			['x'] = 0, 
+			['y'] = 0, 
+			['z'] = 0,
+			['Information'] = '[E] Go Up' 
+		},
+
+		['Exit'] = {
+			['x'] = 0, 
+			['y'] = 0, 
+			['z'] = 0, 
+			['Information'] = '[E] Go Down' 
+		}
+	},]]
+
+	--Next here
+
+
+	['Meth'] = {
+		
+		['Job'] = 'none',
+		['Enter'] = { 
+			['x'] = 1866.0673, 
+			['y'] = -196.98, 
+			['z'] = 249.254,
+			['Information'] = 'Appuyer sur ~y~[E]~s~ pour entrer' 
+		},
+
+
+		
+
+		['Exit'] = {
+			['x'] = 1254.5402, 
+			['y'] = -100.53, 
+			['z'] = 37.39, 
+			['Information'] = 'Appuyer sur ~y~[E]~s~ pour sortir' 
+		}
+	},
+
+
+
+	
+	['weed'] = {
+		['Job'] = 'none',
+		['Enter'] = { 
+			['x'] = 1455.835, 
+			['y'] = -233.66, 
+			['z'] = 45.918,
+			['Information'] = 'Appuyer sur ~y~[E]~s~ pour entrer' 
+		},
+		
+		['Exit'] = {
+			['x'] = 1265.602, 
+			['y'] = -183.47, 
+			['z'] = 48.16, 
+			['Information'] = 'Appuyer sur ~y~[E]~s~ pour sortir' 
+		}
+	},
+
+	
+
+	['blanchisseur'] = {
+		['Job'] = 'none',
+		['Enter'] = { 
+			['x'] = -1413.265, 
+			['y'] = 124.4037, 
+			['z'] = -48.261,
+			['Information'] = 'Appuyer sur ~y~[E]~s~ pour entrer' 
+		},
+		
+		['Exit'] = {
+			['x'] = 737.689, 
+			['y'] = -1198.38, 
+			['z'] = 75.66, 
+			['Information'] = 'Appuyer sur ~y~[E]~s~ pour sortir' 
+		},
+
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
